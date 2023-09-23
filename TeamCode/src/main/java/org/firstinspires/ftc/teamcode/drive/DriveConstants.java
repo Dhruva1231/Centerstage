191aff81214ecd4d2a6229b7bcf62bd9c8bacb1b
Dhruva <dhruvarishi@gmail.com>
2023-09-23 15:52:17 -0400

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -15,7 +15,7 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
  * These are not the only parameters; some are located in the localizer classes, drive base classes,
  * and op modes themselves.
  */
-//Test
+//auto branch test
 @Config
 public class DriveConstants {
 
